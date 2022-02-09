e3c58f2173dc1cacc136c44c9cd67c9f45ca9fd9
GekkeijuITA <98583823+GekkeijuITA@users.noreply.github.com>
2022-02-09 10:24:30 +0100

    Add files via upload

---
+++ generatorePassword.java
@@ -0,0 +1,31 @@
+import java.util.concurrent.ThreadLocalRandom;
+import java.io.*;
+import java.awt.*;
+
+public class generatorePassword {
+    private String password;
+    public String generaPassword(){
+        password="";
+        int i;
+        for(i=0;i<12;i++){            
+            password+=(char)numeroCasuale();
+        }
+        return password;
+    }
+    
+    public int numeroCasuale(){
+        int min=33,max=126;
+        int rand=ThreadLocalRandom.current().nextInt(min, max + 1);
+        return rand;
+    }
+    
+    public void copia(String psw,String utente,File f) throws IOException{//crea un file         
+            Desktop desktop = Desktop.getDesktop();
+            FileWriter w= new FileWriter(f);
+            desktop.open(f);
+            w.write("Nome utente: "+utente+"\n");
+            w.write("Password: "+psw);           
+            w.flush();     
+            w.close();
+    }
+}
